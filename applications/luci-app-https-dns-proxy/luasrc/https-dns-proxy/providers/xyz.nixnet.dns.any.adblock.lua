7cd24af05e29169476e64e3c67d8a70f941a8202
slfox <slfox@inbox.ru>
2020-05-21 00:35:44 +0500

    Create xyz.nixnet.dns.any.adblock.lua

---
+++ applications/luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/xyz.nixnet.dns.any.adblock.lua
@@ -0,0 +1,8 @@
+return {
+	name = "NixNet-Adblock Anycast",
+	label = _("NixNet (Adblock)"),
+	resolver_url = "https://adblock.any.dns.nixnet.xyz/dns-query",
+	bootstrap_dns = "198.251.90.89",
+	help_link = "https://docs.nixnet.services/DNS",
+	help_link_text = "nixnet.services"
+}
